69e85d57eec4dceaaa98bdf3f13e604ce84691ca
rcalegario <rodrigocalegario@gmail.com>
2017-12-31 13:58:08 -0300

    inicial value validator

--- js/financial.js
+++ js/financial.js
@@ -1,8 +1,15 @@
 $('#calculate').click(function() {
-    const value = Number($('#value').val());
-    $('#value-invest').val(value * 0.3);
-    $('#value-individual').val(value * 0.35);
-    $('#value-couple').val(value * 0.2);
-    $('#value-education').val(value * 0.05);
-    $('#value-want').val(value * 0.05);
+    const value = Number($('#value').val().replace(',','.'));
+    if($('#value')[0].validity.valid){
+        $('#value-invest').val(value * 0.3);
+        $('#value-spend').val(value * 0.7);
+        $('#value-individual').val(value * 0.35);
+        $('#value-couple').val(value * 0.2);
+        $('#value-education').val(value * 0.05);
+        $('#value-want').val(value * 0.05);
+        $('#error-value').hide();
+    } else {
+        $('#error-value').show();
+        $('#error-value').text($('#value')[0].validationMessage);
+    }
 });
