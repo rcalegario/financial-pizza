04ae7b29ac2cd7481d56dc451f7811596ee668f3
rcalegario <rodrigocalegario@gmail.com>
2018-08-25 12:24:11 -0300

    desbloqueado a modificação de porcentagens

--- js/financial.js
+++ js/financial.js
@@ -1,12 +1,12 @@
-const percents = [
-    {part: 'invest', percent: 0.3},
-    {part: 'individual', percent: 0.4},
-    {part: 'couple', percent: 0.2},
-    {part: 'education', percent: 0.05},
-    {part: 'want', percent: 0.05} // need to be the last
-];
+// const percents = [
+//     {part: 'invest', percent: 0.3},
+//     {part: 'individual', percent: 0.4},
+//     {part: 'couple', percent: 0.2},
+//     {part: 'education', percent: 0.05},
+//     {part: 'want', percent: 0.05} // need to be the last
+// ];
 
-function calculate(value) {
+function calculate(value, percents) {
     let amount = 0;
     let spend = 0;
     percents.forEach(element => {
@@ -37,11 +37,26 @@ function calculate(value) {
 
 $('#calculate').click(function() {
     const value = Number($('#value').val().replace(',','.'));
-    if($('#value')[0].validity.valid){
-        calculate(value);
-        $('#error-value').hide();
-    } else {
+    const percents = $('.porcent');
+    const percentsValues = [];
+    let percentsTotal = 0;
+    for (let i = 0; i < percents.length; i++) {
+        const pValue = Number(percents[i].value);
+        percentsTotal += pValue;
+        percentValue = { part: percents[i].id.match(/^porcent-(.*)/)[1], percent: (pValue/100)};
+        percentsValues.push(percentValue);
+    }
+
+    $('#error-value').hide();
+    $('#error-percents').hide();
+    if (!$('#value')[0].validity.valid) {
         $('#error-value').show();
         $('#error-value').text($('#value')[0].validationMessage);
+    } else if (percentsTotal !== 100) {
+        $('#error-percents').show();
+        $('#error-percents').text(`As porcentagens não estão com valores corretos (${percentsTotal}%)`);
+    } else {
+        calculate(value, percentsValues);
     }
+    
 });
